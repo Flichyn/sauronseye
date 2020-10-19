73a85971b50af52d756557ca2d03eb4582e3ea32
Flichyn <70525056+Flichyn@users.noreply.github.com>
2020-10-19 22:11:49 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Sauron's Eye</title>
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-1 dark"></div>
+        <div class="col-1 offset-9 dark"></div>
+    </div>
+    <div class="row">
+        <div class="col-1 dark"></div>
+        <div class="col-1 offset-3 orange"></div>
+        <div class="col-1 yellow"></div>
+        <div class="col-1 orange"></div>
+        <div class="col-1 offset-3 dark"></div>
+    </div>
+    <div class="row">
+        <div class="col-1 dark"></div>
+        <div class="col-1 offset-2 orange"></div>
+        <div class="col-1 yellow"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 yellow"></div>
+        <div class="col-1 orange"></div>
+        <div class="col-1 offset-2 dark"></div>
+    </div>
+    <div class="row">
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 orange"></div>
+        <div class="col-1 orange"></div>
+        <div class="col-1 yellow"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 yellow"></div>
+        <div class="col-1 orange"></div>
+        <div class="col-1 orange"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+    </div>
+    <div class="row">
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 offset-1 orange"></div>
+        <div class="col-1 yellow"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 yellow"></div>
+        <div class="col-1 orange"></div>
+        <div class="col-1 offset-1 dark"></div>
+        <div class="col-1 dark"></div>
+    </div>
+    <div class="row">
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 offset-1 orange"></div>
+        <div class="col-1 yellow"></div>
+        <div class="col-1 orange"></div>
+        <div class="col-1 offset-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+    </div>
+    <div class="row">
+        <div class="col-1 offset-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 offset-3 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+    </div>
+    <div class="row">
+        <div class="col-1 offset-2 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+    </div>
+    <div class="row">
+        <div class="col-1 offset-3 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+    </div>
+    <div class="row">
+        <div class="col-1 offset-4 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+    </div>
+    <div class="row">
+        <div class="col-1 offset-4 dark"></div>
+        <div class="col-1 dark"></div>
+        <div class="col-1 dark"></div>
+    </div>
+</div>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+        crossorigin="anonymous"></script>
+</body>
+</html>
